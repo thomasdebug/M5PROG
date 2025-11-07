28fc4a020eb4913aef7d341a68404688d265dcf0
thomasdebug <38764@ma-web.nl>
2025-11-07 18:33:12 +0100

    failed

--- Assets/Scripts/6/Brute.cs
+++ Assets/Scripts/6/Brute.cs
@@ -6,12 +6,12 @@ public class Brute : EnemyParent
 {
     void Start()
     {
-        // Pas eigenschappen aan voor een trage, sterke Brute
+        
         moveSpeed = 1f;
         patrolRange = 2f;
         maxHealth = 10;
 
-        // Startpositie en health resetten net als in parent
+        
         base.Start();
     }
 }
